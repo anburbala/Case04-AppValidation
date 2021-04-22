fe8b45afb17238033fe3aab73875bad5ad231680
anburbala <82209663+anburbala@users.noreply.github.com>
2021-04-22 12:41:54 +0530

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplatedrivenComponent } from './templatedriven/templatedriven.component';
 import {MustMatchDirective} from './must-match.directive';
+import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplatedrivenComponent,
-    MustMatchDirective
-    
-    
+    MustMatchDirective,
+    ReactiveformComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
