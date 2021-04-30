25a9c60a79b2a5161e15cff8d34e0d126e04149f
anburbala <82209663+anburbala@users.noreply.github.com>
2021-04-30 19:13:25 +0530

    auth guard service changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,26 @@ import { TemplatedrivenComponent } from './templatedriven/templatedriven.compone
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 import {CustomerrorComponent} from './customerror/customerror.component';
 import {ProductComponent} from './product/product.component';
-import { AuthGuard } from './auth/auth.guard';
-
-
-
+import { AuthGuardService } from './auth/auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   { path: '', component: TemplatedrivenComponent },
   { path: 'templatedriven-component', component: TemplatedrivenComponent },
   { path: 'reactiveform-component', component: ReactiveformComponent },
-  { path: 'product-component', component: ProductComponent, canActivate : [AuthGuard] },
+  { path: 'product-component', component: ProductComponent, canActivate : [AuthGuardService] },
+  { path: 'login-component', component: LoginComponent},
+  {path: 'logout-component', component : LogoutComponent},
+  {path: 'aboutus-component', loadChildren: () => import('./aboutus/aboutus.module')
+  .then(m => m.AboutUsModule)},
   { path: '**',component : CustomerrorComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule {
 
