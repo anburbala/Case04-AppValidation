1c44d8b1f2f96280e3adec5b380f19d017e2c7e8
anburbala <82209663+anburbala@users.noreply.github.com>
2021-05-02 22:34:55 +0530

    model and service changes

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,14 +1,17 @@
 export class Product { 
  
-    constructor(productID:number,    name: string ,   price:number) {
+    constructor(productID:number,    name: string ,   price:number, desc : string) {
         this.productID=productID;
         this.name=name;
         this.price=price;
+        this.Desc=desc;
+        
     }
  
     productID:number ;
     name: string ;
     price:number;
+    Desc : string;
  
 }
  
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,5 @@
 import {Product} from '../model/Product'
-import { of, Observable, throwError} from 'rxjs';
+import { of, Observable} from 'rxjs';
  
 export class ProductService{
  
@@ -7,12 +7,12 @@ export class ProductService{
     
     public constructor() {
         this.products=[
-            new Product(1000,'Memory Card',500),
-            new Product(2000,'Pen Drive',750),
-            new Product(3000,'Power Bank',100),
-            new Product(4000,'Computer',100),
-            new Product(5000,'Laptop',100),
-            new Product(6000,'Printer',100),
+            new Product(1000,'Memory Card',500, 'test product'),
+            new Product(2000,'Pen Drive',750,'test product'),
+            new Product(3000,'Power Bank',100,'test product'),
+            new Product(4000,'Computer',100,'test product'),
+            new Product(5000,'Laptop',100,'test product'),
+            new Product(6000,'Printer',100,'test product'),
         ]
     }
  
